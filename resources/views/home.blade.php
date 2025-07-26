78acd40a3b00a8e5c5aaf731a2dcc77f98cd14fa
GiuseppeDeno <giuseppedeno@gmail.com>
2025-07-26 15:20:29 +0200

    home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -47,20 +47,7 @@
 
 
     <!-- Recent Articles -->
-  {{--   <section id="articles" class="recent-articles">
-        <div class="container">
-            <h2 class="section-title">Articoli Recenti</h2>
-            <div class="row">
-                
-                @foreach ($articlesRec as $article)
-                 {{--   <x-cardsRecent :article='$article'></x-cardsRecent> --}}
-{{-- 
-                   <x-card :article='$article'  type="recent"/>
-                @endforeach
-
-            </div>
-        </div>
-    </section>  tutto sostituito da recent-articles    --}} 
+ 
 
      <x-recent-articles :articles-rec="$articlesRec" />
 
