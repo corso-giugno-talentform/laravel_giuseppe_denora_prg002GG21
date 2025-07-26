7b08c39dcb6b9b53fd315b8dff221b7d6abf9497
GiuseppeDeno <giuseppedeno@gmail.com>
2025-07-26 19:44:35 +0200

    web.php

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[PageController::class ,'homepage'])->name('homepage');
 /* Route::get('/article/{slug}', [PageController::class ,'article'])->name('article');
 Route::get('/article/{slug}', [PageController::class ,'articleEvid'])->name('article'); */
-Route::get('/article/evidenza/{slug}', [PageController::class, 'articleEvid']);
-Route::get('/article/recent/{slug}', [PageController::class, 'article']);
+Route::get('/article/evidenza/{slug}', [PageController::class, 'articleEvid'])->name('article');
+Route::get('/article/recent/{slug}', [PageController::class, 'article'])->name('article');
 
 
